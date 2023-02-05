c7fb47ddf2470e028b57132ec421c3c483e60c13
Justin Mitchell <15881261+mitchjus01@users.noreply.github.com>
2023-02-05 17:23:21 -0500

    Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,23 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let text = ""
 
+    if (string === "") {
+        return "";
+    }
+    
+    for (let i = 0; i < num; i++) {
+        if (num === 0) {
+            text = "";
+        } else {
+            text += string;
+        }
+    }
+
+    if (num < 0) {
+        return "ERROR";
+    } else {
+        return text;
+    }
 };
 
 // Do not edit below this line
